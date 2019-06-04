8e735f064856647db54d5fc82e158077b069e18a
Joffrey CK <gnemo.ka@gmail.com>
2019-06-04 17:23:16 +0200

    Testing multi module library

---
+++ widget/src/main/java/com/aleengo/android/material/widget/ToastUtil.java
@@ -0,0 +1,31 @@
+package com.aleengo.android.material.widget;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.StringRes;
+
+/**
+ * Copyright (c) Aleengo, 2019. All rights reserved.
+ * Created by bau.cj on 03/06/2019.
+ */
+public final class ToastUtil {
+
+    private ToastUtil() {}
+
+    public static void toast(Context context, @StringRes int resId) {
+        toast(context, resId, Toast.LENGTH_SHORT);
+    }
+
+    public static void toast(Context context, String message) {
+        toast(context, message, Toast.LENGTH_SHORT);
+    }
+
+    public static void toast(Context context, @StringRes int resId, int duration) {
+        Toast.makeText(context, resId, duration).show();
+    }
+
+    public static void toast(Context context, String message, int duration) {
+        Toast.makeText(context, message, duration).show();
+    }
+}
